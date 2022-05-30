dbc6a9ac68706b7d7bf39e2cb2b1ea775fff01b3
Abdelrahman Salem <Abdelrahman_Salem@comcast.com>
2022-05-30 14:16:17 -0400

    Remove container

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,8 +2,8 @@ import { Feature } from '../components/Feature'
 import { Navbar } from '../components/Navbar'
 
 export const App = () => (
-  <div className="container">
+  <>
     <Navbar />
     <Feature />
-  </div>
+  </>
 )
